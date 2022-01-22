18beb95032af5a63ec15ebfb2dcec4cd57079694
seanflyyy <wenhan.er@akribis-sys.com>
2022-01-22 22:45:05 +0800

    allow user to login, sign up, auto login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,14 @@ class SessionsController < ApplicationController
                 .find_by(email: params["user"]["email"])
                 .try(:authenticate, params["user"]["password"])
         if user 
+            payload = {user_id: user.id}
+            token = encode_token(payload)
             session[:user_id] = user.id
             render json: {
                 status: :created, 
                 logged_in: true, 
-                user: user
+                user: user,
+                token: token
             }
         else 
             render json: {
@@ -21,9 +24,12 @@ class SessionsController < ApplicationController
 
     def logged_in 
         if @current_user
+            payload = {user_id: @current_user.id}
+            token = encode_token(payload)
             render json: {
                 logged_in: true,
-                user: @current_user
+                user: @current_user,
+                token: token
             }
         else 
             render json: {
