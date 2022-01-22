b5a865ced22a61062c65114103f6ac4dcbf37eb3
seanflyyy <wenhan.er@akribis-sys.com>
2022-01-22 10:48:48 +0800

    enable login, logout and register

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,20 @@
+class RegistrationController < ApplicationController 
+    def create 
+        user = User.create!(
+            email: params['user']['email'], 
+            password: params['user']['password'],
+            password_confirmation: params['user']['password_confirmation']
+        )
+    end 
+
+    if user
+        session[:user_id] = user.id
+        render json: {
+            status: :created,
+            user: user
+        }
+    else 
+        render json: {status: 500} # status 500 is an error
+    end 
+
+end 
