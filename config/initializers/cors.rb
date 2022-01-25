c510e6432ce29e2c9182f09851b4fe5b044dece8
seanflyyy <wenhan.er@akribis-sys.com>
2022-01-25 19:15:20 +0800

    added main site

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,5 +9,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
         credentials: true
     end 
+    allow do 
+        origins "https://trusting-hawking-bdc7d2.netlify.app/"
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end 
 end
 
