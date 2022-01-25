f0ccb7670d0f6b3c9d529accca51492212e6cadf
seanflyyy <wenhan.er@akribis-sys.com>
2022-01-25 22:20:41 +0800

    updated route

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         credentials: true
     end 
     allow do 
-        origins "https://trusting-hawking-bdc7d2.netlify.app/"
+        origins "https://trusting-hawking-bdc7d2.netlify.app"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
         credentials: true
     end 
