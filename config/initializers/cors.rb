09a9eb09e51e6c40c03a7a49483da062b409f164
seanflyyy <wenhan.er@akribis-sys.com>
2022-01-25 20:34:22 +0800

    allow all routes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,18 +1,23 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
     allow do 
-        origins "http://localhost:3000/"
+        origins "*"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
         credentials: true
     end 
-    allow do 
-        origins "https://61efd3b6d5a539e551b747ff--trusting-hawking-bdc7d2.netlify.app/"
-        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
-    end 
-    allow do 
-        origins "https://trusting-hawking-bdc7d2.netlify.app/"
-        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
-    end 
+    # allow do 
+    #     origins "http://localhost:3000/"
+    #     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    #     credentials: true
+    # end 
+    # allow do 
+    #     origins "https://61efd3b6d5a539e551b747ff--trusting-hawking-bdc7d2.netlify.app/"
+    #     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    #     credentials: true
+    # end 
+    # allow do 
+    #     origins "https://trusting-hawking-bdc7d2.netlify.app/"
+    #     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    #     credentials: true
+    # end 
 end
 
